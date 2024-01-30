5b232bce3d3c6d0f138554316f090d3e46c70a6c
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-01-31 02:14:22 +0530

    removed the bugs

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,6 +1,5 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
-import { param } from "jquery";
 import { NextResponse } from "next/server";
 
 export async function PATCH (
